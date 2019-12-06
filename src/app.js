3111e77fed807aaa4bf6f9d29b3e571e14ad3bf2
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2019-12-06 11:46:12 -0800

    added DELETE request and POST body validations. Updated logger

--- src/app.js
+++ src/app.js
@@ -3,11 +3,9 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
-const uuid = require('uuid/v4');
 
 const { NODE_ENV } = require("./config");
 const bookmarkRouter = require("./bookmark-router");
-
 
 const app = express();
 
@@ -19,21 +17,16 @@ app.use(cors());
 app.use(express.json());
 app.use(bookmarkRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
-
 app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
 
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-      return res.status(401).json({ error: 'Unauthorized request'})
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
   }
 
-  next()
-})
-
+  next();
+});
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
--- src/bookmark-router.js
+++ src/bookmark-router.js
@@ -6,57 +6,85 @@ const bodyParser = express.json();
 const logger = require("./logger");
 const bookmarks = require("./dataStore");
 
-bookmarkRouter.route("/bookmarks").get((req, res) => {
-  res.json(bookmarks);
-});
+bookmarkRouter
+  .route("/bookmarks")
+  .get((req, res) => {
+    res.json(bookmarks);
+  })
+  .post(bodyParser, (req, res) => {
+    const { title, url, description, rating } = req.body;
 
-bookmarkRouter.route("/bookmarks/:id").get((req, res) => {
-  const { id } = req.params;
-  const bookmark = bookmarks.find(bm => bm.id == id);
+    if (!title || title.trim().length < 1) {
+      logger.error("Title is required");
+      return res.status(400).send("Title must be longer than 1 character");
+    }
 
-  if (!bookmark) {
-    logger.error(`Bookmark with ${id} not found.`);
-    return res.status(404).send("Bookmark Not Found");
-  }
-  res.json(bookmark);
-});
+    if (!url || url.trim().length < 5 || !url.includes("http")) {
+      logger.error("URL is required");
+      return res
+        .status(400)
+        .send(
+          "Url must include protocol and be at least 5 characters in length"
+        );
+    }
 
-bookmarkRouter.route("/bookmarks").post(bodyParser, (req, res) => {
-  const { title, url, description, rating } = req.body;
+    if (description && description.trim().length < 1) {
+      logger.error("If description provided, must have at least one character");
+      return res
+        .status(400)
+        .send("If description provided, must have at least one character");
+    }
 
-  if (!title) {
-    logger.error("Title is required");
-    return res.status(400).send("Invalid data");
-  }
+    if ((rating && rating < 1) || rating > 5) {
+      logger.error("Rating should be number between 1 and 5, if provided");
+      return res
+        .status(400)
+        .send("Rating should be number between 1 and 5, if provided");
+    }
 
-  if (!url) {
-    logger.error("URL is required");
-    return res.status(400).send("Invalid data");
-  }
+    const id = uuid();
 
-  if (!description) {
-    logger.error("Description is required");
-    return res.status(400).send("Invalid data");
-  }
+    const bookmark = {
+      id,
+      title,
+      url,
+      description,
+      rating
+    };
 
-  if (!rating) {
-    logger.error("Rating is required");
-    return res.status(400).send("invalid data");
-  }
+    bookmarks.push(bookmark);
 
-  const id = uuid();
+    logger.info(`Bookmark with id ${id} created.`);
+    res
+      .status(201)
+      .location(`http://localhost:8080/bookmarks/${id}`)
+      .json({ id });
+  });
 
-  const bookmark = {
-    id,
-    title,
-    url,
-    description,
-    rating
-  };
+bookmarkRouter
+  .route("/bookmarks/:id")
+  .get((req, res) => {
+    const { id } = req.params;
+    const bookmark = bookmarks.find(bm => bm.id == id);
 
-  bookmarks.push(bookmark);
+    if (!bookmark) {
+      logger.error(`Bookmark with ${id} not found.`);
+      return res.status(404).send("Bookmark Not Found");
+    }
+    res.json(bookmark);
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    const bmIndex = bookmarks.findIndex(bm => bm.id == id);
 
-  res.status(201).json(bookmark);
-});
+    if (bmIndex === -1) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res.status(404).send("Not Found");
+    }
+
+    bookmarks.splice(bmIndex, 1);
+    logger.info(`Bookmark with id ${id} deleted.`);
+    res.status(204).end();
+  });
 
 module.exports = bookmarkRouter;
